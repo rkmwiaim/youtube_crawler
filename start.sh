7b4600ace26f33ee828ad8cc93daa3e4c6a0b843
kmryu <rkmwiaim1@gmail.com>
2020-02-16 01:41:29 +0000

    add scripts

---
+++ start.sh
@@ -0,0 +1,2 @@
+. /home/archive/youtube_crawler/venv/bin/activate
+python /home/archive/youtube_crawler/YoutubeCrawler.py
---
+++ start_with_log.sh
@@ -0,0 +1,1 @@
+/home/archive/youtube_crawler/start.sh > /home/archive/youtube_crawler/logs/$(date '+%Y-%m-%d').log 2>&1
